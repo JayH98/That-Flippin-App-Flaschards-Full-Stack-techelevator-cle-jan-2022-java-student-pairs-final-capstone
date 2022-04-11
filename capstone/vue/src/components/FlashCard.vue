c16298b07314c2ad2688c97eb2d4023e72aaa5f2
Angel Scott <angel.v.scott@gmail.com>
2022-04-11 16:34:43 -0400

    Added FlashCardList.vue & Flashcard.vue to control display of user's flashcards

---
+++ capstone/vue/src/components/FlashCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class = "flashcard">
+      <p>{{flashcard.id}}</p>
+      <p>{{}}
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'reading-list',
+    props: ['flashcard'],
+    components: {
+        
+    }
+}
+</script>
+
+<style>
+.flashcard-container {
+    display:flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+}
+</style>
---
+++ capstone/vue/src/components/FlashCardList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flashcard-container">
+      <flash-card v-bind:flashcard="flashcard" v-for="flashcard in $store.state.flashcards" v-bind:key="flashcards.id"/>
+  </div>
+</template>
+
+<script>
+import FlashCard from '@component/FlashCard.vue'
+
+export default {
+    name: 'flashcard-list',
+    components: {
+        FlashCard
+        
+    }
+}
+</script>
+
+<style>
+.flashcard-container {
+    display:flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+}
+</style>
