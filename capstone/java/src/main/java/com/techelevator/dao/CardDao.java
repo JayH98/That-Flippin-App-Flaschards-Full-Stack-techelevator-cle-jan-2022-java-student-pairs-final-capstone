d0d786cf949ed2ef50cf014754afd8c19011eed2
dan gregg <danggregg@gmail.com>
2022-04-12 21:55:12 -0400

    See No Red, Hear No Red, Speak No Red

--- capstone/java/src/main/java/com/techelevator/dao/CardDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/CardDao.java
@@ -4,5 +4,11 @@ import com.techelevator.model.Card;
 
 
 public interface CardDao {
+        public Card findCardByTag(String tag);
+        public Card findCardByCreator(String creator);
+        public Card findCardByModule(int module);
+        public Card findCardByDeck(String deck);
+        public Card editCard(String tag, String question, String answer);
+
 
 }
